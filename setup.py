7007572cecbcd6d6f8d09bba4c48aaeb58589f83
Adriana <adriigarr@gmail.com>
2023-11-01 18:13:49 +0100

    tensor Calculator exercise

--- setup.py
+++ setup.py
@@ -6,18 +6,15 @@ A brief comment just to modify the file
 """
 
 from setuptools import setup, find_packages
-from class_exercise import __author__,__version__,__name__
+from class_exercise import __name__
 
 
-VERSION = __version__
-AUTHOR = __author__
+
 NAME = __name__
 
 setup(
     name                    = NAME,
-    version                 = VERSION,
     description             = 'Brief description of your package',
-    author                  = AUTHOR,
     author_email            = 'adriigarr@gmail.com',
     license                 = 'MIT',
     python_requires         = '>=3.9.5',
